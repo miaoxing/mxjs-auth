6977c8cb122b4861b022661a0b4d86d580d35d7e
twinh <twinhuang@qq.com>
2024-12-25 23:34:30 +0800

    feat(auth, experimental): 移除不再使用的 `filterChildren` 方法

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 import { AuthProvider, useAuth } from './AuthProvider';
 import Access, { withAccess } from './Access';
-// @experimental
-import filterChildren from './filterChildren';
 
 export {
   AuthProvider,
   useAuth,
   Access,
   withAccess,
-  filterChildren,
 };
